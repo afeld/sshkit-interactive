8f90e74e53f21285eb8d3c202c1963db875119d8
Florian Schwab <me@ydkn.de>
2016-09-12 03:44:58 +0200

    prevent using _on_ if running interactively

--- lib/sshkit/interactive/dsl.rb
+++ lib/sshkit/interactive/dsl.rb
@@ -2,7 +2,17 @@ module SSHKit
   module Interactive
     module DSL
       def run_interactively(host, &block)
+        Thread.current[:run_interactively] = true
+
         SSHKit::Interactive::Backend.new(host, &block).run
+      ensure
+        Thread.current[:run_interactively] = false
+      end
+
+      def on(*args)
+        raise 'Switching host in interactive mode is not possible' if Thread.current[:run_interactively]
+
+        super
       end
     end
   end
